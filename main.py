7af9487dcad99f775891fca45e177ef7350781f5
DS <darensin01@gmail.com>
2021-01-22 15:35:01 +0800

    updated log function

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ def log_command_pressed(update):
     username = user.username
     user_id = user.id
     first_name = user.first_name
-    command = update.message.text[:255]
 
     if update.message.location is not None:
         lat = update.message.location.latitude
         long = update.message.location.longitude
         logging.info(f"{username}/{first_name} ({user_id}) sent a location: {lat}, {long}.")
     else:
+        command = update.message.text[:255]
         logging.info(f"{username}/{first_name} ({user_id}) sent {command}.")
 
 
